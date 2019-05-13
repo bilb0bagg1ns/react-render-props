f056635dd0604b85d4c3a8aeb52cec938a522305
bilbobagg1ns <mprasad@regis.edu>
2019-05-13 09:33:47 -0600

    Initilize Repository

---
+++ docs/MyReadme.txt
@@ -0,0 +1,3 @@
+
+Source: React's Render Props Pattern - Children as a Function
+https://www.robinwieruch.de/react-render-props-pattern/
--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Amount from "./Amount";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Amount>
+        {amount => (
+          <div>
+            <Pound amount={amount} />
+            <Euro amount={amount} />
+          </div>
+        )}
+      </Amount>
+    );
+  }
 }
 
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
 export default App;
